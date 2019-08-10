bd2cbca368e51cee9589d14868fd29e7b9005537
tonymatheus <tony.chaves14@gmail.com>
2019-08-10 10:10:51 -0300

    Criando a View do  Projeto

--- meuProjeto/contas/views.py
+++ meuProjeto/contas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import  datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html ="<html><body>It is Now %s.</body></html>"% now
+    return HttpResponse(html)
+
