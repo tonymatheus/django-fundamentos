22207f3401ad4b37601de3c4190c1c6cfbf6cdee
tonymatheus <tony.chaves14@gmail.com>
2019-08-10 12:27:10 -0300

    Criando a Model Categoria

--- meuProjeto/contas/admin.py
+++ meuProjeto/contas/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
-from .models import  categoria
-admin.site.register(categoria)
+from .models import  Categoria
+from .models import  Transacao
+admin.site.register(Categoria)
+admin.site.register(Transacao)
--- meuProjeto/contas/models.py
+++ meuProjeto/contas/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-class categoria(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+class Transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_length=7, max_digits= 10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes=models.TextField(null=True , blank= True)
+
+    class Meta:
+     verbose_name_plural= 'Tensações'
+
+    def __str__(self):
+        return self.descricao
